9b6d4dcee832d69a8934820c54cfd9c58e2986ce
austinbenedicto <austinbenedicto271.com>
2025-04-28 06:33:51 -0400

    changed evolers ideas

--- Evolvers/evolver.java
+++ Evolvers/evolver.java
@@ -1,23 +1,59 @@
 package Evolvers;
 
-public abstract class evolver
-{
-    public int evoStatus;
+public abstract class Evolver {
+    protected String name;
+    protected int maxLives;
+    protected int currentLives;
+    protected int strength;
+    protected int defense;
 
-    public int health;
-
-    public int attackStatus;
-    public int attackLevel;
-    public int attackPower;
-
-    public int magicStatus;
-    public int magicLevel;
-    public int magicPower;
-
-    public int getEvoStatus()
-    {
-        return evoStatus;
+    public Evolver(String name, int maxLives, int strength, int defense) {
+        this.name = name;
+        this.maxLives = maxLives;
+        this.currentLives = maxLives;
+        this.strength = strength;
+        this.defense = defense;
     }
-    public abstract void setEvoStatus(int evoStatus);
 
+    // Called when the Evolver dies and returns stronger
+    public void evolve() {
+        if (currentLives > 0) {
+            currentLives--;
+            strengthenOnDeath();
+        }
+    }
+
+    // Abstract method to define how each Evolver strengthens uniquely
+    protected abstract void strengthenOnDeath();
+
+    // Example: Attack action
+    public int attack() {
+        return strength;
+    }
+
+    // Example: Defend action
+    public int defend() {
+        return defense;
+    }
+
+    public boolean isAlive() {
+        return currentLives > 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCurrentLives() {
+        return currentLives;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
 }
+
