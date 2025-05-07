901f7d56c349191170347126f1f2f270d1e2af74
austinbenedicto <austinbenedicto271.com>
2025-05-07 06:45:30 -0400

    added more spells types

---
+++ Cards/Spells/MillSpells.java
@@ -0,0 +1,9 @@
+package Cards.Spells;
+
+public abstract class MillSpells extends Spell
+{
+    public MillSpells(String name, String description) 
+    {
+        super(name, description);
+    }
+}
---
+++ Cards/Spells/SummonSpells.java
@@ -0,0 +1,10 @@
+package Cards.Spells;
+
+public class SummonSpells extends Spell
+{
+    public SummonSpells(String name, String description) 
+    {
+        super(name, description);
+    }
+    
+}
